a2981b4b79452fb421487e7002eb9572d9b98ceb
John Richardson <richardsonjm@gmail.com>
2014-03-27 13:05:11 -0400

    Split seeds by environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Environment specific seeds
+seed_file = Rails.root.join( 'db', 'seeds', "#{Rails.env}.rb")
+load(seed_file) if File.exists?(seed_file)
