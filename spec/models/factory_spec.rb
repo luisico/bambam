d97aa4292093c2f2af7f9b766a41d1da9ef87040
John Richardson <richardsonjm@gmail.com>
2014-03-27 11:55:52 -0400

    Test all factories

---
+++ spec/models/factory_spec.rb
@@ -0,0 +1,14 @@
+# See https://github.com/thoughtbot/factory_girl/wiki/Testing-all-Factories-%28with-RSpec%29
+require 'spec_helper'
+
+describe 'validate FactoryGirl factories' do
+  FactoryGirl.factories.each do |factory|
+    context "with factory for :#{factory.name}" do
+      subject { FactoryGirl.build(factory.name) }
+
+      it "is valid" do
+        subject.valid?.should be, subject.errors.full_messages.join(", ")
+      end
+    end
+  end
+end
