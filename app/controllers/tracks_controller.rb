1a27c71ea8bbd24af48ffb671b2e67d8a905f4f6
John Richardson <richardsonjm@gmail.com>
2014-04-17 16:56:28 -0400

    remove json responses from track controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,6 +2,8 @@ class TracksController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
 
+  respond_to :html
+
   def index
   end
 
@@ -15,35 +17,24 @@ class TracksController < ApplicationController
   end
 
   def create
-    respond_to do |format|
-      if @track.save
-        format.html { redirect_to @track, notice: 'Track was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @track }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @track.errors, status: :unprocessable_entity }
-      end
+    if @track.save
+      redirect_to @track, notice: 'Track was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   def update
-    respond_to do |format|
-      if @track.update(track_params)
-        format.html { redirect_to @track, notice: 'Track was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @track.errors, status: :unprocessable_entity }
-      end
+    if @track.update(track_params)
+      redirect_to @track, notice: 'Track was successfully updated.'
+    else
+     render action: 'edit'
     end
   end
 
   def destroy
     @track.destroy
-    respond_to do |format|
-      format.html { redirect_to tracks_url }
-      format.json { head :no_content }
-    end
+    redirect_to tracks_url
   end
 
   private
