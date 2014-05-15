78beef12b7e3efacd6a863160bb6f2da4a25acba
John Richardson <richardsonjm@gmail.com>
2014-05-15 15:43:57 -0400

    Take forward slashes out of track factory

--- spec/factories/tracks.rb
+++ spec/factories/tracks.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :track do
     sequence(:name) {|n| "track#{n}"}
-    sequence(:path) {|n| "/zenodotus/track#{n}"}
+    sequence(:path) {|n| File.join "", "zenodotus", "track#{n}"}
   end
 end
