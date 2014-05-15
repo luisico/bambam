16a70cc43d6a8c22ad42dc95d1aeddc244e37b56
John Richardson <richardsonjm@gmail.com>
2014-05-15 15:43:35 -0400

    Add validates_path_of to track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,7 @@
 class Track < ActiveRecord::Base
   validates_presence_of :name, :path
+  validates_path_of :path, within: [File.join('', 'zenodotus', 'abc', 'store', 'projects', 'Ivashkivlab'),
+                                    File.join('', 'zenodotus', 'dat01', 'ivashkivlab_store'),
+                                    'tmp',
+                                    File.join('', 'zenodotus') ]
 end
