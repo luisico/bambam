be6f563d9bc21679c70b095f9480a784e1529e95
John Richardson <richardsonjm@gmail.com>
2014-06-09 14:07:35 -0400

    Add specs to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :members, :through => :memberships,
                      :class_name => 'User',
                      :foreign_key => 'user_id',
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -17,4 +17,43 @@ describe Group do
     it { should respond_to :name }
     it { should validate_presence_of(:name) }
   end
+
+  describe "owner" do
+    it { should respond_to :owner }
+  end
+
+  describe "associations" do
+    context "memberships" do
+      it { should have_many :memberships }
+      it { should respond_to :memberships }
+      it { should respond_to :membership_ids }
+    end
+
+    context "members" do
+      it { should have_many :members }
+      it { should respond_to :members }
+      it { should respond_to :member_ids }
+    end
+
+    context "owners" do
+      it { should belong_to :owner }
+      it { should respond_to :owner }
+    end
+  end
+
+  describe "when group destroyed" do
+    before do
+      @group.members << @group.owner
+      @group.save!
+    end
+
+    it "should destroy the group" do
+      expect { @group.destroy }.to change(Group, :count).by(-1)
+      expect { Group.find(@group.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "should destroy associated memberships" do
+      expect { @group.destroy }.to change(Membership, :count).by(-1)
+    end
+  end
 end
