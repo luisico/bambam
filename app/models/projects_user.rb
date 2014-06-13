ffcb31339780d7f142d3088cfd37a452502224bf
John Richardson <richardsonjm@gmail.com>
2014-06-13 10:06:05 -0400

    Create projects_user model

---
+++ app/models/projects_user.rb
@@ -0,0 +1,4 @@
+class ProjectsUser < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :user
+end
---
+++ spec/models/projects_user_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ProjectsUser do
+  before { @projects_user = FactoryGirl.build(:projects_user) }
+
+  subject { @projects_user }
+
+  describe "database fields" do
+    it_behaves_like "timestampable table"
+    it { should have_db_column(:user_id) }
+    it { should have_db_column(:project_id) }
+  end
+
+  describe "user_id" do
+    it { should respond_to :user_id }
+  end
+
+  describe "project_id" do
+    it { should respond_to :project_id }
+  end
+
+  describe "associations" do
+    context "users" do
+      it { should belong_to :user }
+      it { should respond_to :user }
+      it { should respond_to :user_id }
+    end
+
+    context "projects" do
+      it { should belong_to :project }
+      it { should respond_to :project }
+      it { should respond_to :project_id }
+    end
+  end
+end
