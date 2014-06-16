ffe655a0e9c16da7f7066333e9a33ffde6775723
Luis Gracia <lgraval@gmail.com>
2014-06-16 10:41:33 -0400

    Refactor seeds for groups

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,11 +1,11 @@
 # Admin and users
-FactoryGirl.create(:admin)
-FactoryGirl.create(:inviter)
-FactoryGirl.create_list(:user, 3)
+admin = FactoryGirl.create(:admin)
+inviter = FactoryGirl.create(:inviter)
+users = FactoryGirl.create_list(:user, 3)
 
 # Groups & membership
-FactoryGirl.create(:group)
-FactoryGirl.create(:membership)
+FactoryGirl.create(:group, members: [admin, inviter, users[0], users[1]])
+FactoryGirl.create(:group, members: [inviter, users[2]])
 
 # Tracks
 FactoryGirl.create_list(:track, 3)
