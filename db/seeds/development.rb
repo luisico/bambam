7dbc29e72ed83475c557274600575c414f41639a
John Richardson <richardsonjm@gmail.com>
2014-06-16 11:26:37 -0400

    fix group seed

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -4,8 +4,8 @@ inviter = FactoryGirl.create(:inviter)
 users = FactoryGirl.create_list(:user, 3)
 
 # Groups & membership
-FactoryGirl.create(:group, members: [admin, inviter, users[0], users[1]])
-FactoryGirl.create(:group, members: [inviter, users[2]])
+FactoryGirl.create(:group, owner: users[0], members: [admin, inviter, users[0], users[1]])
+FactoryGirl.create(:group, owner: inviter, members: [inviter, users[2]])
 
 # Tracks
 FactoryGirl.create_list(:track, 3)
