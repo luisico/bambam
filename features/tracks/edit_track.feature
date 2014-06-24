637d35029a9005c6138cf866720ea76737346cfc
John Richardson <richardsonjm@gmail.com>
2014-06-24 14:59:19 -0400

    Update edit track feature

--- features/tracks/edit_track.feature
+++ features/tracks/edit_track.feature
@@ -5,19 +5,28 @@ Feature: Edit track information
 
   Background:
     Given I am signed in
+    And I own a project
+    And there is a track in that project
 
   Scenario: User can access the track edit page from the track show page
-    Given there is a track in the system
     When I am on the track page
     And I click on the track edit link
     Then I should be on the edit track page
 
-  Scenario: User can change the track name
-    Given there is a track in the system
+  Scenario: User can change the track name on the track edit page
     When I visit the edit track page
     Then I should be able to edit the track name
 
-  Scenario: User can change the track path
-    Given there is a track in the system
+  Scenario: User can change the track path on the track edit page
     When I visit the edit track page
     Then I should be able to edit the track path
+
+  @javascript
+  Scenario: User can change the track name on the project edit page
+    When I visit the edit project page
+    Then I should be to udpate the track name on the project edit page
+
+  @javascript
+  Scenario: User can change the track path on the project edit page
+    When I visit the edit project page
+    Then I should be to udpate the track path on the project edit page
--- features/tracks/step_definitions/edit_track_steps.rb
+++ features/tracks/step_definitions/edit_track_steps.rb
@@ -1,4 +1,9 @@
 ### Methods
+def build_track_path
+  @path = File.join 'tmp', 'tests', 'new_track_path.bam'
+  FileUtils.mkdir(File.join('tmp', 'tests')) if !File.exist?(File.join('tmp', 'tests'))
+  File.open(@path, 'w') {|f| f.puts 'file content' }
+end
 
 ### Given
 
@@ -36,4 +41,28 @@ Then /^I should be able to edit the track path$/ do
     @track.reload
   }.to change(@track, :path)
   expect(page).to have_css('.alert-box', text: 'Track was successfully updated')
+end
+
+Then /^I should be to udpate the track name on the project edit page$/ do
+  track = @project.tracks.first
+  expect {
+    click_link 'edit'
+    execute_script %Q{$('.track-form-group .track-form-fields').show();}
+    fill_in 'project[tracks_attributes][0][name]', with: 'new_track_name'
+    click_button 'Update'
+    track.reload
+  }.to change(track, :name)
+  expect(page).to have_css('.alert-box', text: 'Project was successfully updated')
+end
+
+Then /^I should be to udpate the track path on the project edit page$/ do
+  build_track_path
+  track = @project.tracks.first
+  expect {
+    click_link 'edit'
+    fill_in 'project[tracks_attributes][0][path]', with: @path
+    click_button 'Update'
+    track.reload
+  }.to change(track, :path)
+  expect(page).to have_css('.alert-box', text: 'Project was successfully updated')
 end
