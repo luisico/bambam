0890f4342546fd8eac99133f57d49eac2d864cf9
John Richardson <richardsonjm@gmail.com>
2014-06-27 15:39:23 -0400

    fix seed file

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -12,5 +12,5 @@ FactoryGirl.create(:group, owner: admin2, members: [admin1, users[2]])
 tracks = FactoryGirl.create_list(:track, 3)
 
 # Projects & projects_users
-FactoryGirl.create(:project, owner: users[0], users: [admin, inviter, users[0], users[1]], tracks: [tracks[0], tracks[2]])
+FactoryGirl.create(:project, owner: users[0], users: [admin1, inviter, users[0], users[1]], tracks: [tracks[0], tracks[2]])
 FactoryGirl.create(:project, owner: inviter, users: [inviter, users[2]], tracks: [tracks[1]])
