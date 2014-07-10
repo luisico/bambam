6f26ded2e9c329adcb10eb602f2ec911b09a0880
John Richardson <richardsonjm@gmail.com>
2014-07-10 14:45:03 -0400

    Uncomment projects routing specs

--- spec/routing/projects_routing_spec.rb
+++ spec/routing/projects_routing_spec.rb
@@ -7,9 +7,9 @@ describe "Projects" do
       assert_routing({ path: '/projects/1', method: :get },     { controller: 'projects', action: 'show', id: '1' })
       assert_routing({ path: '/projects/new', method: :get },   { controller: 'projects', action: 'new' })
       assert_routing({ path: '/projects', method: :post },      { controller: 'projects', action: 'create' })
-      # assert_routing({ path: '/projects/1/edit', method: :get}, { controller: 'projects', action: 'edit', id: '1' })
-      # assert_routing({ path: '/projects/1', method: :patch},    { controller: 'projects', action: 'update', id: '1'})
-      # assert_routing({ path: '/projects/1', method: :delete},   { controller: 'projects', action: 'destroy', id: '1'})
+      assert_routing({ path: '/projects/1/edit', method: :get}, { controller: 'projects', action: 'edit', id: '1' })
+      assert_routing({ path: '/projects/1', method: :patch},    { controller: 'projects', action: 'update', id: '1'})
+      assert_routing({ path: '/projects/1', method: :delete},   { controller: 'projects', action: 'destroy', id: '1'})
     end
   end
 end
