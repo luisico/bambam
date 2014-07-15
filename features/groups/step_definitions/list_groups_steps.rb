03dd8e5fb7c5648d0cf75e3ed293ce4b30548085
John Richardson <richardsonjm@gmail.com>
2014-07-15 14:24:58 -0400

    Add sheriff badge and remove 'owner' from group list partial

--- features/groups/step_definitions/list_groups_steps.rb
+++ features/groups/step_definitions/list_groups_steps.rb
@@ -9,7 +9,11 @@
 Then /^I should see a list of all groups$/ do
   expect(Group.count).to be > 0
   Group.all.each do |group|
-    expect(page).to have_content "#{group.name} owned by #{group.owner.email}"
+    within("#group_#{group.id}") do
+      expect(page).to have_content group.name
+      expect(page).to have_css('.fi-sheriff-badge')
+      expect(page).to have_content group.owner.email
+    end
   end
 end
 
@@ -18,9 +22,13 @@ Then /^I should only see a list of groups I am a member of$/ do
   Group.all.each do |group|
     text = "#{group.name} owned by #{group.owner.email}"
     if group.members.include?(@user)
-      expect(page).to have_content text
+      within("#group_#{group.id}") do
+        expect(page).to have_content group.name
+        expect(page).to have_css('.fi-sheriff-badge')
+        expect(page).to have_content group.owner.email
+      end
     else
-      expect(page).not_to have_content text
+      expect(page).not_to have_css("#group_#{group.id}")
     end
   end
 end
