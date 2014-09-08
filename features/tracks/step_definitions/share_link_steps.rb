b6112f57be3a9d05d2014db19c533b4bfd3a8058
Luis Gracia <lgraval@gmail.com>
2014-09-08 13:16:55 -0400

    Remove sleep calls from cucumber steps

--- features/tracks/step_definitions/share_link_steps.rb
+++ features/tracks/step_definitions/share_link_steps.rb
@@ -39,11 +39,12 @@ end
 Then /^I should be able to create a shareable link$/ do
   expect{
     click_link "Create new share link"
-    expect(page).not_to have_content "Create new share link"
+    expect(page).not_to have_link "Create new share link"
     within('.new_share_link') {
       fill_in 'share_link[expires_at]', with: Time.now + 3.days
       click_button 'Create Share link'
     }
+    expect(page).to have_link "Create new share link"
   }.to change(ShareLink, :count).by(1)
 end
 
@@ -51,11 +52,12 @@ Then /^I should be able to create a link that expires in "(.*?)"$/ do |time|
   expiration = expiration_date(time)
   expect{
     click_link "Create new share link"
-    expect(page).not_to have_content "Create new share link"
+    expect(page).not_to have_link "Create new share link"
     within('.new_share_link') {
       click_link time
       click_button 'Create Share link'
     }
+    expect(page).to have_link "Create new share link"
   }.to change(ShareLink, :count).by(1)
   expect(ShareLink.last.expires_at.to_s[0..9]).to eq expiration.to_s
 end
@@ -63,10 +65,11 @@ end
 Then /^I should be able to create a shareable link without entering date$/ do
   expect{
     click_link "Create new share link"
-    expect(page).not_to have_content "Create new share link"
+    expect(page).not_to have_link "Create new share link"
     within('.new_share_link') {
       click_button 'Create Share link'
     }
+    expect(page).to have_link "Create new share link"
   }.to change(ShareLink, :count).by(1)
 end
 
@@ -93,11 +96,11 @@ end
 Then /^I should be able to cancel the creation a shareable link$/ do
   expect{
     click_link "Create new share link"
-    expect(page).not_to have_content "Create new share link"
+    expect(page).not_to have_link "Create new share link"
     within('.new_share_link') {
-      fill_in 'share_link[expires_at]', with: Time.now + 3.days
       click_link 'Cancel'
     }
+    expect(page).to have_link "Create new share link"
   }.not_to change(ShareLink, :count)
   expect(page).not_to have_css('.new_share_link')
 end
@@ -105,7 +108,7 @@ end
 Then /^I should not be able to create a shareable link with expired date$/ do
   expect{
     click_link "Create new share link"
-    expect(page).not_to have_content "Create new share link"
+    expect(page).not_to have_link "Create new share link"
     within('.new_share_link') {
       fill_in 'share_link[expires_at]', with: DateTime.yesterday
       click_button 'Create Share link'
@@ -128,7 +131,7 @@ Then /^I should be able to delete the share link$/ do
   expect{
     page.evaluate_script('window.confirm = function() { return true; }')
     click_link "delete_share_link_#{@share_link.id}"
-    sleep 1
+    expect(page).not_to have_link "delete_share_link_#{@share_link.id}"
   }.to change(ShareLink, :count).by(-1)
 end
 
@@ -138,8 +141,8 @@ Then /^I should be able to renew the share link$/ do
     within(".edit_share_link") {
       fill_in 'share_link[expires_at]', with: Time.now + 5.days
       click_button 'Update Share link'
-      sleep 1
     }
+    expect(page).not_to have_css ".edit_share_link"
     @share_link.reload
   }.to change(@share_link, :expires_at)
 end
@@ -151,8 +154,8 @@ Then /^I should be able to renew the link with date that expires in "(.*?)"$/ do
     within(".edit_share_link") {
       click_link time
       click_button 'Update Share link'
-      sleep 1
     }
+    expect(page).not_to have_css ".edit_share_link"
     @share_link.reload
   }.to change(@share_link, :expires_at)
   expect(@share_link.expires_at.to_s[0..9]).to eq expiration.to_s
@@ -162,7 +165,6 @@ Then /^I should be able to cancel the renewal the share link$/ do
   expect{
     click_link "edit_link_#{@share_link.id}"
     within(".edit_share_link") {
-      fill_in 'share_link[expires_at]', with: Time.now + 5.days
       click_link 'Cancel'
     }
     @share_link.reload
@@ -175,7 +177,6 @@ Then /^I should not be able to renew the share link with expired date$/ do
     within(".edit_share_link") {
       fill_in 'share_link[expires_at]', with: DateTime.yesterday
       click_button 'Update Share link'
-      sleep 1
     }
     @share_link.reload
   }.not_to change(@share_link, :expires_at)
@@ -197,7 +198,7 @@ Then /^I should be able to delete the expired share link$/ do
   expect{
     click_link "delete_share_link_#{@expired_share_link.id}"
     page.evaluate_script('window.confirm = function() { return true; }')
-    sleep 1
+    expect(page).not_to have_link "delete_share_link_#{@expired_share_link.id}"
   }.to change(ShareLink, :count).by(-1)
 end
 
