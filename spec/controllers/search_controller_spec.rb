9b4a048f6f9b9b21448c2aa0f0f21db60e145998
John Richardson <richardsonjm@gmail.com>
2014-09-17 15:19:58 -0400

    add assignment test to search controller spec. Fixes #46

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,27 +1,37 @@
 require 'spec_helper'
 
 describe SearchController do
-  describe "Post 'search'" do
-    before do
-      @project = FactoryGirl.create(:project)
-      @track = FactoryGirl.create(:test_track, project: @project)
-      @user = FactoryGirl.create(:user, projects: [@project])
-    end
-
+  describe "Get 'search'" do
     context "as a signed in user" do
-      before { sign_in @user }
+      before do
+        @projects = [FactoryGirl.create(:project, name: "best_project")]
+        @tracks = [FactoryGirl.create(:test_track, name: "best_track", project: @projects.first)]
+        @groups = [FactoryGirl.create(:group, name: "best_group")]
+        @users = [FactoryGirl.create(:user, email: "best_user@example.com", projects: @projects, groups: @groups)]
+        sign_in @users.first
+      end
 
       context "with valid parameters" do
         it "should be a success" do
-          post :search, q: 'best'
+          get :search, q: 'best'
           expect(response).to be_success
+        end
+
+        it "should return only valid search results" do
+          [:project, :test_track, :group, :user].each do |model|
+            FactoryGirl.create(model)
+          end
+          get :search, q: 'best'
+          [:projects, :tracks, :groups, :users].each do |col|
+            expect(assigns(col)).to eq instance_variable_get("@#{col.to_s}")
+          end
         end
       end
     end
 
     context "as a visitor" do
       it "should redirect to the sign in page" do
-        post :search, q: 'best'
+        get :search, q: 'best'
         expect(response).not_to be_success
         expect(response).to redirect_to new_user_session_url
       end
