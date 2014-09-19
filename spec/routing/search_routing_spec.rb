663a0d4d6159a0a49d12daed64b4aaa4024b52fe
John Richardson <richardsonjm@gmail.com>
2014-09-19 11:15:30 -0400

    fixed misspelled test file

--- spec/routing/search_routing_spec.rb
+++ spec/routing/search_routing_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe 'Search' do
   describe "routes to" do
     it "the search route" do
-      assert_routing({ path: '/search', method: :post }, { controller: 'search' })
+      assert_routing({ path: '/search', method: :get }, { controller: 'search', action: 'search' })
     end
   end
 end
