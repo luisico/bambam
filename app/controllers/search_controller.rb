d7712ae9c6063bc4a497eb20ba2c45d962cd4948
John Richardson <richardsonjm@gmail.com>
2014-09-22 10:47:36 -0400

    Working code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,15 @@ class SearchController < ApplicationController
 
   def search
     @q = params[:q]
-    @groups   = Group.accessible_by(current_ability).search(name_cont: @q).result
+    projects = Project.accessible_by(current_ability).search(name_cont: @q).result
     @tracks   = Track.accessible_by(current_ability).search(name_cont: @q).result
-    @projects = Project.accessible_by(current_ability).search(name_cont: @q).result
+    tracks_projects = []
+    @tracks.each {|track| tracks_projects << track.project}.uniq
+    @projects = projects | tracks_projects
+    groups   = Group.accessible_by(current_ability).search(name_cont: @q).result
     @users    = User.search(email_cont: @q).result
+    users_groups = []
+    @users.each {|user| users_groups << user.groups }.uniq
+    @groups = groups | users_groups.flatten
   end
-
 end
