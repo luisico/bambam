29851f8de322bfdff5fde3087532763c6dbe84a4
Luis Gracia <lgraval@gmail.com>
2014-11-03 13:53:24 -0500

    Fix test for igv tooltips

--- features/tracks/step_definitions/track_steps.rb
+++ features/tracks/step_definitions/track_steps.rb
@@ -1,11 +1,14 @@
 Then /^I should be able to activate a tooltip on the IGV button(s)?$/ do |plural|
+  selector = "//a[contains(@class, 'button') and contains(text(), 'igv')]/span[contains(@class, 'has-tip-icon')]"
   if plural
-    all('.igv-tip').each do |tooltip|
+    tooltips = all(:xpath, selector)
+    expect(tooltips.count).not_to eq 0
+    all('.has-tip-icon', text: 'igv').each do |tooltip|
       tooltip.trigger(:mouseover)
       expect(page).to have_content "IGV must be open on your computer"
     end
   else
-    find('.igv-tip').hover
+    find(:xpath, selector).hover
     expect(page).to have_content "IGV must be open on your computer"
   end
 end
