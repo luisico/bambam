1c151fa74b8e8d2b8a470b2841ba606e7dfdf93c
John Richardson <richardsonjm@gmail.com>
2014-11-18 11:22:12 -0500

    Change names in user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com"}
-    sequence(:first_name) {|n| "first_name#{n}"}
-    sequence(:last_name) {|n| "last_name#{n}"}
+    sequence(:first_name) {|n| "Firstname#{n}"}
+    sequence(:last_name) {|n| "Lastname#{n}"}
     password '123456789'
     password_confirmation '123456789'
 
