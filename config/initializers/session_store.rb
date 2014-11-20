861f9ba57f74cd028ab7320a552e3e2755fcc973
John Richardson <richardsonjm@gmail.com>
2014-11-20 10:24:30 -0500

    Remove string concatenation from session store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Bambam::Application.config.session_store :cookie_store, key: ENV['SESSION_STORE_KEY'] + "_session"
+Bambam::Application.config.session_store :cookie_store, key: ENV['SESSION_STORE_KEY']
