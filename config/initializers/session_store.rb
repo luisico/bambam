bd40ac730fa77a7956587ec01ed1f3decbcef9c3
John Richardson <richardsonjm@gmail.com>
2014-11-20 11:40:41 -0500

    Remove ENV variable from session store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
+fullapp = ""
+[2,4].each {|i| fullapp << "_#{Rails.root.to_s.split(File::SEPARATOR)[i]}"}
 
-Bambam::Application.config.session_store :cookie_store, key: ENV['SESSION_STORE_KEY']
+Bambam::Application.config.session_store :cookie_store, key: fullapp + "_session"
