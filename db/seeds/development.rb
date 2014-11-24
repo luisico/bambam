7b0f53ae37f96167d607ed54652918c4e9ffce3a
John Richardson <richardsonjm@gmail.com>
2014-11-24 15:21:28 -0500

    Refactor development seed file

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,16 +1,16 @@
 # Admin and users
 admin1 = FactoryGirl.create(:admin)
-admin2 = FactoryGirl.create(:admin)
-manager = FactoryGirl.create(:manager)
+manager1 = FactoryGirl.create(:manager)
+manager2 = FactoryGirl.create(:manager)
 users = FactoryGirl.create_list(:user, 3)
 
 # Groups & membership
-FactoryGirl.create(:group, owner: admin1, members: [manager, users[0], users[1]])
-FactoryGirl.create(:group, owner: admin2, members: [admin1, users[2]])
+FactoryGirl.create(:group, owner: manager1, members: [manager2, users[0], users[1]])
+FactoryGirl.create(:group, owner: manager2, members: [manager1, users[2]])
 
 # Projects & projects_users
-project1 = FactoryGirl.create(:project, owner: admin1, users: [admin1, manager, users[0], users[1]])
-project2 = FactoryGirl.create(:project, owner: admin2, users: [manager, users[2]])
+project1 = FactoryGirl.create(:project, owner: manager1, users: [manager2, users[0], users[1]])
+project2 = FactoryGirl.create(:project, owner: manager2, users: [manager1, users[2]])
 
 # Tracks
 FactoryGirl.create_list(:track, 3, project: project1)
