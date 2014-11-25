f645e7c75d0c2ae81acc263412b9b35e36827f0e
Luis Gracia <lgraval@gmail.com>
2014-11-25 15:38:34 -0500

    Improve seeds by adding a user without name and use roles as last name

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -3,14 +3,15 @@ admin1 = FactoryGirl.create(:admin)
 manager1 = FactoryGirl.create(:manager)
 manager2 = FactoryGirl.create(:manager)
 users = FactoryGirl.create_list(:user, 3)
+users << FactoryGirl.create(:user, :unnamed)
 
 # Groups & membership
 FactoryGirl.create(:group, owner: manager1, members: [manager2, users[0], users[1]])
-FactoryGirl.create(:group, owner: manager2, members: [manager1, users[2]])
+FactoryGirl.create(:group, owner: manager2, members: [users[2], users[3]])
 
 # Projects & projects_users
 project1 = FactoryGirl.create(:project, owner: manager1, users: [manager2, users[0], users[1]])
-project2 = FactoryGirl.create(:project, owner: manager2, users: [manager1, users[2]])
+project2 = FactoryGirl.create(:project, owner: manager2, users: [users[2], users[3]])
 
 # Tracks
 FactoryGirl.create_list(:track, 3, project: project1)
