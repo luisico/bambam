bbd0e7de06553946a2a0ca39a24586177d5e2e31
John Richardson <richardsonjm@gmail.com>
2014-12-10 13:23:18 -0500

    Correct text on datapath routing spec

--- spec/routing/datapaths_routing_spec.rb
+++ spec/routing/datapaths_routing_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe "Datapaths" do
   describe "routes to" do
-    it "a complete set of restful actions" do
+    it "all restful actions except show" do
       assert_routing({ path: '/datapaths', method: :get },        { controller: 'datapaths', action: 'index' })
       assert_routing({ path: '/datapaths/new', method: :get },    { controller: 'datapaths', action: 'new' })
       assert_routing({ path: '/datapaths/1/edit', method: :get},  { controller: 'datapaths', action: 'edit', id: '1' })
