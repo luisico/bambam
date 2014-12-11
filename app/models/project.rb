2e8bb564c9e66d792871bf11a34841759b40faa4
John Richardson <richardsonjm@gmail.com>
2014-12-11 14:25:32 -0500

    Allowed datapath method on Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,15 @@ class Project < ActiveRecord::Base
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
   validates_presence_of :name
   accepts_nested_attributes_for :tracks, allow_destroy: true
+
+  def allowed_paths
+    self.projects_datapaths.collect do |project_datapath|
+      master_datapath = Datapath.find(project_datapath.datapath_id).path
+      if project_datapath.sub_directory.present?
+        File.join master_datapath, project_datapath.sub_directory
+      else
+        master_datapath
+      end
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -51,6 +51,34 @@ describe Project do
     it { should respond_to :track_ids }
   end
 
+  describe "#allowed_datapaths" do
+    it "should return an array of allowed datapaths for the project" do
+      master1 = FactoryGirl.create(:test_datapath)
+      master2 = FactoryGirl.create(:test_datapath)
+      FactoryGirl.create(:projects_datapath,
+                          datapath: master1,
+                          project: @project,
+                          sub_directory: "")
+      projects_datapath = FactoryGirl.create(:projects_datapath,
+                                              datapath: master1,
+                                              project: @project)
+      FactoryGirl.create(:projects_datapath,
+                          datapath: master2,
+                          project: @project,
+                          sub_directory: "")
+      results = [
+        master1.path,
+        File.join(master1.path, projects_datapath.sub_directory),
+        master2.path
+      ]
+      expect(@project.allowed_paths).to eq results
+    end
+
+    it "should return an empty array when the project has no datapaths" do
+      expect(@project.allowed_paths).to eq []
+    end
+  end
+
   describe "when project destroyed" do
     before {@project.save!}
 
