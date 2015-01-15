829e1b842a07f908c5bdeb0e6a0cb9f76804467e
John Richardson <richardsonjm@gmail.com>
2015-01-15 09:41:59 -0500

    Make key datapath id for master datapaths

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -37,7 +37,6 @@ class TracksController < ApplicationController
   end
 
   def generate_tree(datapaths=[])
-    @key = 0
     formats = %w(.bw .bam)
     tree = []
 
@@ -49,10 +48,10 @@ class TracksController < ApplicationController
       if files.empty?
         parent = add_node_to_tree(tree, datapath.path)
         parent[:folder] = true
-        parent[:key] = datapath.path
+        parent[:key] = datapath.id
       else
         files.each do |file|
-          parent = add_node_to_tree(tree, datapath.path, true)
+          parent = add_node_to_tree(tree, datapath.path, true, datapath.id)
           file.sub!(common, '').split(File::SEPARATOR)[2..-1].each do |item|
             parent = add_node_to_tree(parent, item)
           end
@@ -63,7 +62,7 @@ class TracksController < ApplicationController
     tree
   end
 
-  def add_node_to_tree(tree, child, expanded=false)
+  def add_node_to_tree(tree, child, expanded=false, id=nil)
     if tree.is_a? Array
       parent = tree
     else
@@ -74,7 +73,8 @@ class TracksController < ApplicationController
     node = parent.select{|c| c[:title] == child}
 
     if node.empty?
-      node = {title: child, key: @key+=1}
+      node = {title: child}
+      node.merge!(key: id) if id
       node.merge!(expanded: true) if expanded
       tree[:folder] = true unless tree.is_a? Array
       parent << node
