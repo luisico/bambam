c30d4cb202898778bdf591c4e9810bb18fa6bbe4
John Richardson <richardsonjm@gmail.com>
2015-02-17 10:25:20 -0500

    Remove unused features, scenarios and steps from project features

--- features/projects/new_project.feature
+++ features/projects/new_project.feature
@@ -4,15 +4,6 @@ Feature: Create a project
   As a manager
   I want to be able to access a form where I can add a new project
 
-  # @javascript
-  # Scenario: Manager goes to new project page
-  #   Given I am signed in as a manager
-  #   And there are 3 other users in the system
-  #   And I am on the new project page
-  #   Then I should see myself listed as project owner
-  #   And I should see a list of potential users
-  #   And I should see a link to "Add a track"
-
   Scenario: Manager creates a new project
     Given I am signed in as a manager
     When I am on the projects page
@@ -26,35 +17,7 @@ Feature: Create a project
     When I am on the projects page
     Then I should not be able to create a new project without a name
 
-  # @javascript
-  # Scenario: Manager can add multiple users to a project
-  #   Given I am signed in as a manager
-  #   And there are 3 other users in the system
-  #   And I am on the new project page
-  #   When I create a project with multiple users
-  #   Then I should be on the project show page
-  #   And I should see the project's users with profile links
-  #   And I should see a message that the project was created successfully
-
-  # @javascript
-  # Scenario: Manager can add multiple tracks to a project
-  #   Given I am signed in as a manager
-  #   And there is 1 other user in the system
-  #   And I am on the new project page
-  #   When I create a project with multiple tracks
-  #   Then I should be on the project show page
-  #   And I should see the project's tracks
-  #   And I should see a message that the project was created successfully
-
   Scenario: Manager can cancel out of project new page
     Given I am signed in as a manager
     When I am on the projects page
     Then I should be able to cancel new project
-
-  # @javascript
-  # Scenario: Manager can delete a track before creating project
-  #   Given I am signed in as a manager
-  #   And there is 1 other user in the system
-  #   When I am on the new project page
-  #   And I delete a track before creating project
-  #   Then I should not create a new track
--- features/projects/step_definitions/new_project_steps.rb
+++ features/projects/step_definitions/new_project_steps.rb
@@ -20,65 +20,9 @@ def submit_project_form(options={})
   end
 end
 
-# def add_track_to_project
-#   click_link 'Add a track'
-#   within('.new-record') {
-#     fill_track_form
-#     expect(page).not_to have_content "Assign track to a project"
-#   }
-# end
-
 ### Given
 
 ### When
-
-# When /^I create a new project with a user and a track$/ do
-#   expect{
-#     build_project
-#     add_track_to_project
-#     fill_project_form
-#   }.to change(Project, :count).by(1)
-# end
-
-# When /^I create a project without a name$/ do
-#   expect{
-#     build_project
-#     fill_in 'Project name', with: @project_attrs.merge(name: '')[:name]
-#     click_button 'Create Project'
-#   }.not_to change(Project, :count)
-# end
-
-# When /^I create a project with multiple users$/ do
-#   expect {
-#     build_project
-#     fill_project_form do
-#       fill_in_select2("project_user_ids", with: User.all[-2].handle)
-#     end
-#   }.to change(Project, :count).by(1)
-# end
-
-# When /^I create a project with multiple tracks$/ do
-#   expect {
-#     build_project
-#     add_track_to_project
-#     click_link 'Add another track'
-#     second_track = page.all(:css, '.new-record')[1]
-#     within(second_track) {
-#       fill_track_form
-#     }
-#     fill_project_form
-#   }.to change(Project, :count).by(1)
-# end
-
-# When /^I delete a track before creating project$/ do
-#   expect {
-#     build_project
-#     add_track_to_project
-#     find('.remove-track').trigger('click')
-#     fill_project_form
-#   }.to change(Project, :count).by(1)
-#   expect(current_path).to eq project_path(Project.last)
-# end
 
 ### Then
 
@@ -111,29 +55,6 @@ Then /^I should be able to cancel new project$/ do
   expect(page).to have_link "New Project"
 end
 
-# Then /^I should( not)? see myself listed as project owner$/ do |negate|
-#   current_user = (@admin || @manager)
-#   if negate
-#     within('#project-owner') {
-#       expect(page).not_to have_content current_user.handle
-#     }
-#   else
-#     within('#project-owner') {
-#       expect(page).to have_content current_user.handle
-#     }
-#   end
-# end
-
-# Then /^I should see a list of potential users$/ do
-#   find("#s2id_project_user_ids").click
-#   within(".select2-results") {
-#     expect(page).not_to have_content @manager.handle
-#     @users.each do |user|
-#       expect(page).to have_content user.handle
-#     end
-#   }
-# end
-
 Then /^I should see a message that the project was created successfully$/ do
   expect(page).to have_content('Project was successfully created')
 end
@@ -145,22 +66,3 @@ end
 Then /^I should be the project's owner$/ do
   expect(Project.last.owner).to eq(@manager)
 end
-
-# Then /^I should see all project members$/ do
-#   expect(page).to have_content Project.last.owner.handle
-#   expect(page).to have_content User.last.handle
-#   expect(page).to have_content User.all[-2].handle
-# end
-
-# Then /^I should see all the project tracks$/ do
-#   project = @project || Project.last
-#   expect(project.tracks.count).to be > 0
-#   project.tracks.each do |track|
-#     expect(page).to have_link track.name
-#   end
-# end
-
-# Then /^I should not create a new track$/ do
-#   expect(Track.count).to eq(0)
-#   expect(page).not_to have_link @track[:name]
-# end
