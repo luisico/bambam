69c1224592fcf9fa10eb785795b64cbb677a2125
Luis Gracia <lgraval@gmail.com>
2015-02-17 16:16:25 -0500

    Refactor edit project's users features

--- features/projects/step_definitions/edit_users_steps.rb
+++ features/projects/step_definitions/edit_users_steps.rb
@@ -12,18 +12,21 @@ Then /^I should( not)? be able to change users in the project$/ do |negate|
   else
     click_link "Edit Project Users"
     deleted = []
+
     expect {
-      @project.users[-2..-1].each{ |u| remove_from_select2(u.handle_with_email); deleted << u }
-      @users.each{ |u| fill_in_select2("project_user_ids", with: u.handle)}
+      @project.users[-2..-1].each { |u| remove_from_select2(u.handle_with_email); deleted << u }
+      @users.each { |u| fill_in_select2("project_user_ids", with: u.handle)}
       click_button 'Update'
       expect(page).to have_content "Read-Only Users"
       @project.reload
     }.to change(@project.users, :count).by(1)
     expect(current_path).to eq project_path(@project)
+
     deleted.each do |u|
       expect(@project.users).not_to include(u)
       expect(page).not_to have_content(u.handle)
     end
+
     @users.each do |u|
       expect(@project.users).to include(u)
       expect(page).to have_content(u.handle)
@@ -38,6 +41,7 @@ Then /^I should be able to cancel out of the edit project user form$/ do
       expect(page).to have_content "Edit Project Users"
       click_link "Cancel"
     }
-  }.not_to change(@project.users, :count)
+    @project.reload
+  }.not_to change(@project, :users)
   expect(page).to have_content "Read-Only Users"
 end
