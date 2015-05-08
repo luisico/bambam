8b7ded84e36b5f63373ede49ba5873d1ce0850e0
Luis Gracia <lgraval@gmail.com>
2015-05-08 12:03:49 -0400

    Display track genomes in projects. Fixes #121

--- features/projects/step_definitions/show_project_steps.rb
+++ features/projects/step_definitions/show_project_steps.rb
@@ -22,6 +22,7 @@ Then /^I should see the project's tracks$/ do
   project = @project || Project.last
   project.tracks.each do |track|
     expect(page).to have_link track.name
+    expect(page).to have_content track.genome
     expect(page).to have_selector(:xpath, "//a[contains(@href, 'http://localhost:60151/load') and text()='igv']")
   end
 end
