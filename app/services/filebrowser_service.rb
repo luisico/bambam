33a8084241b2965a85864cda6ef45d322c69df8c
Luis Gracia <lgraval@gmail.com>
2015-05-13 12:22:38 -0400

    Create basic filebrowser service

---
+++ app/services/filebrowser_service.rb
@@ -0,0 +1,40 @@
+class FilebrowserService
+  FORMATS = %w(bw bam)
+
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def call
+    Dir.chdir(path) do
+      globs = ["*/"].concat(FORMATS.map{ |f| "*.#{f}" })
+      @entries = Dir.glob(globs)
+    end
+
+    entries
+  end
+
+  def to_fancytree
+    fancytree = []
+
+    entries.each do |entry|
+      if m = entry.match(/(.*)\/$/)
+        # Directories
+        node = {title: m[1]}
+        node.merge!(folder: true)
+      else
+        # Files
+        node = {title: entry}
+      end
+      fancytree << node
+    end
+
+    fancytree
+  end
+
+  def entries
+    @entries || call
+  end
+end
