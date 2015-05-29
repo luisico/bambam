839574dab1ec3f39f2d6486d33a33ebb33d15318
Luis Gracia <lgraval@gmail.com>
2015-05-29 12:08:05 -0400

    more work on siblings

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -28,5 +28,7 @@ FactoryGirl.create_list(:track, 3, projects_datapath: projects_datapath1, owner:
 FactoryGirl.create_list(:track, 3, projects_datapath: projects_datapath2, owner: users[1])
 track = FactoryGirl.create(:track, projects_datapath: projects_datapath3, owner: users[1])
 FactoryGirl.create(:track, projects_datapath: projects_datapath3, owner: users[1],
- name: 'track_sibling.bam', path: File.join(track.path.split(File::SEPARATOR)[0...-1], 'sibling', 'track_sibling.bam'))
+  name: 'track_sibling1.bam', path: File.join(track.path.split(File::SEPARATOR)[0...-2], 'track_sibling1.bam'))
+FactoryGirl.create(:track, projects_datapath: projects_datapath3, owner: users[1],
+  name: 'track_sibling2.bam', path: File.join(track.path.split(File::SEPARATOR)[0...-1], 'sibling', 'track_sibling2.bam'))
 FactoryGirl.create_list(:track, 3, projects_datapath: projects_datapath4, owner: users[2])
