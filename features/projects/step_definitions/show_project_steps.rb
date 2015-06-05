278ee832da7cd1e5b39169f67d58db213dd0bf39
John Richardson <richardsonjm@gmail.com>
2015-06-05 14:10:42 -0400

    Show message about adding datapaths for project managers and users

--- features/projects/step_definitions/show_project_steps.rb
+++ features/projects/step_definitions/show_project_steps.rb
@@ -30,6 +30,16 @@ Then /^I should see a section titled "(.*?)"$/ do |title|
   }
 end
 
+Then /^I should see a note about contacting "(.*?)"$/ do |contact|
+  within(find(".subhead-note")){
+    if contact == "admin"
+      expect(page).to have_link "administrator"
+    elsif contact == "owner"
+      expect(page).to have_link "project owner"
+    end
+  }
+end
+
 Then /^I should see the project's tracks$/ do
   project = @project || Project.last
   project.tracks.each do |track|
