01bb77fe47acccdea713024df1d65817ae2fa82a
Luis Gracia <lgraval@gmail.com>
2015-06-09 14:07:26 -0400

    More reorganization of icons styling

--- app/assets/javascripts/hopscotch/tour_project_index.js
+++ app/assets/javascripts/hopscotch/tour_project_index.js
@@ -4,13 +4,13 @@ var tour_project_index = {
   steps: [
     {
       title: "Tour: Project index page",
-      content: "List of all the projects and their summary information.<br/><br/>Click <i class='fi-x small-margin-left margin-right text-color-grey'></i> to exit tour at any time.",
+      content: "List of all the projects and their summary information.<br/><br/>Click <i class='tour-close'></i> to exit tour at any time.",
       target: ".project-tile",
       placement: "right"
     },
     {
       title: "Project index page",
-      content: "Each tile contains a link to the project page, track count <i class='track-icon small-margin-left'></i>, user count <i class='users-icon small-margin-left'></i>, project owner <i class='owner-icon small-margin-left'></i> and date of last update <i class='fi-clock small-margin-left'></i>.",
+      content: "Each tile contains a link to the project page, track count <i class='track-icon icon-tour'></i>, user count <i class='users-icon icon-tour'></i>, project owner <i class='owner-icon icon-tour'></i> and date of last update <i class='fi-clock icon-tour'></i>.",
       target: ".project-tile",
       placement: "right",
       arrowOffset: "center"
