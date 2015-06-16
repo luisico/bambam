4944c8c1762284b6eaf63d7d0fd5314b9446ad04
John Richardson <richardsonjm@gmail.com>
2015-06-16 12:17:50 -0400

    Preselect track helper

--- spec/features/fancytree_spec.rb
+++ spec/features/fancytree_spec.rb
@@ -5,13 +5,21 @@ RSpec.feature "fancytree" do
     @manager = FactoryGirl.create(:manager)
     @project = FactoryGirl.create(:project, owner: @manager)
     @datapaths = create_datapaths
+
     add_user_to_datapaths(@manager, @datapaths)
-    preselect_datapath(@project, @datapaths[0], 'subdir1')
+    sign_in @manager
   end
 
   scenario "Manager views selected datapath on project page", js: true do
-    sign_in @manager
+    preselect_datapath(@project, @datapaths[0], 'subdir1')
     visit project_path(@project)
     expect(page).to have_text 'subdir1'
+  end
+
+  scenario "Manager views selected track on project page", js: true do
+    projects_datapath = preselect_datapath(@project, @datapaths[0], 'subdir2')
+    preselect_track(projects_datapath, 'track4', 'bam', @manager)
+    visit project_path(@project)
+    expect(page).to have_text 'track4.bam'
   end
 end
--- spec/support/tracks_helpers.rb
+++ spec/support/tracks_helpers.rb
@@ -6,5 +6,15 @@ module Tracks
       Pathname.new(path).dirname.mkpath
       FileUtils.cp File.join(Rails.root, 'spec', 'data', 'tracks', file), path
     end
+
+    def preselect_track(projects_datapath, name, ext, owner)
+      formats = %w(*.bw *.bam)
+      globs = formats.map{ |f| File.join(projects_datapath.full_path, "**", f) }
+      files = Dir.glob(globs)
+
+      track_path = files.select {|file| Pathname.new(file).basename.to_s == name + '.' + ext}.join
+      path = track_path.gsub(projects_datapath.full_path + '/', "")
+      FactoryGirl.create(:track, projects_datapath: projects_datapath, path: path, name: name, owner: owner)
+    end
   end
 end
