1f2b734343b2075ac2acd270df3c9523c0a70dd2
John Richardson <richardsonjm@gmail.com>
2015-06-16 14:41:05 -0400

    One datapath per file hieararchy

--- spec/features/fancytree_spec.rb
+++ spec/features/fancytree_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature "fancytree" do
+RSpec.feature "One datapath per file hierarchy" do
   before do
     @manager = FactoryGirl.create(:manager)
     @project = FactoryGirl.create(:project, owner: @manager)
     @datapaths = create_datapaths
 
     add_user_to_datapaths(@manager, @datapaths)
+    preselect_datapath(@project, @datapaths[0], 'subdir1')
+
     sign_in @manager
   end
 
-  scenario "Manager views selected datapath on project page", js: true do
-    preselect_datapath(@project, @datapaths[0], 'subdir1')
+  scenario "manager selects parent of selected datapath", js: true do
     visit project_path(@project)
-    expect(page).to have_text 'subdir1'
-  end
+    expect(fancytree_parent("subdir1")[:class]).to include 'fancytree-selected'
 
-  scenario "Manager views selected track on project page", js: true do
-    projects_datapath = preselect_datapath(@project, @datapaths[0], 'subdir2')
-    preselect_track(projects_datapath, 'track4', 'bam', @manager)
-    visit project_path(@project)
-    expect(page).to have_text 'track4.bam'
+    expect {
+      select_node(@datapaths[0].path)
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.not_to change(@project.projects_datapaths, :count)
+    expect(fancytree_parent("subdir1")[:class]).not_to include 'fancytree-selected'
+    expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
   end
 end
