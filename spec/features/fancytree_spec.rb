bc2ea34c2dfb7b48644437476ba4c518d00a9cea
John Richardson <richardsonjm@gmail.com>
2015-06-17 12:06:12 -0400

    remove @subdir instance variable

--- spec/features/fancytree_spec.rb
+++ spec/features/fancytree_spec.rb
@@ -7,12 +7,12 @@ RSpec.feature "One datapath per file hierarchy" do
     @datapaths = create_datapaths
 
     add_user_to_datapaths(@manager, @datapaths)
-    @subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
 
     sign_in @manager
   end
 
   scenario "manager selects parent of selected datapath", js: true do
+    subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
     visit project_path(@project)
     expect(fancytree_parent("subdir1")[:class]).to include 'fancytree-selected'
 
@@ -26,12 +26,13 @@ RSpec.feature "One datapath per file hierarchy" do
   end
 
   scenario "manager selects parent of selected datapath with tracks", js: true do
+    subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
     %w[track1 track2].each do |name|
-      preselect_track(@subdir1, name, 'bam', @manager)
+      preselect_track(subdir1, name, 'bam', @manager)
     end
 
     @project.tracks.each do |track|
-      expect(track.projects_datapath).to eq @subdir1
+      expect(track.projects_datapath).to eq subdir1
     end
 
     visit project_path(@project)
@@ -46,7 +47,7 @@ RSpec.feature "One datapath per file hierarchy" do
     }.not_to change(@project.projects_datapaths, :count)
 
     @project.tracks.each do |track|
-      expect(track.projects_datapath).not_to eq @subdir1
+      expect(track.projects_datapath).not_to eq subdir1
     end
 
     expect(fancytree_parent("subdir1")[:class]).not_to include 'fancytree-selected'
@@ -56,6 +57,7 @@ RSpec.feature "One datapath per file hierarchy" do
   end
 
   scenario "manager selects parent of two selected datapaths", js: true do
+    subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
     subdir2 = preselect_datapath(@project, @datapaths[0], 'subdir2')
 
     visit project_path(@project)
@@ -76,8 +78,9 @@ RSpec.feature "One datapath per file hierarchy" do
   end
 
   scenario "manager selects parent of two selected datapaths with tracks", js: true do
+    subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
     %w[track1 track2].each do |name|
-      preselect_track(@subdir1, name, 'bam', @manager)
+      preselect_track(subdir1, name, 'bam', @manager)
     end
 
     subdir2 = preselect_datapath(@project, @datapaths[0], 'subdir2')
