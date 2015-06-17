2dc02209b3275987692d265ddd362addd145fed1
John Richardson <richardsonjm@gmail.com>
2015-06-17 11:12:19 -0400

    One datapath per file hiearchy with tracks

--- spec/features/fancytree_spec.rb
+++ spec/features/fancytree_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "One datapath per file hierarchy" do
     @datapaths = create_datapaths
 
     add_user_to_datapaths(@manager, @datapaths)
-    preselect_datapath(@project, @datapaths[0], 'subdir1')
+    @subdir1 = preselect_datapath(@project, @datapaths[0], 'subdir1')
 
     sign_in @manager
   end
@@ -20,7 +20,38 @@ RSpec.feature "One datapath per file hierarchy" do
       select_node(@datapaths[0].path)
       loop until page.evaluate_script('jQuery.active').zero?
     }.not_to change(@project.projects_datapaths, :count)
+
     expect(fancytree_parent("subdir1")[:class]).not_to include 'fancytree-selected'
     expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
+  end
+
+  scenario "manager selects parent of selected datapath with tracks", js: true do
+    %w[track1 track2].each do |name|
+      preselect_track(@subdir1, name, 'bam', @manager)
+    end
+
+    @project.tracks.each do |track|
+      expect(track.projects_datapath).to eq @subdir1
+    end
+
+    visit project_path(@project)
+    %w[subdir1 track1.bam track2.bam].each do |title|
+      expect(fancytree_parent(title)[:class]).to include 'fancytree-selected'
+    end
+
+    expect {
+      select_node(@datapaths[0].path)
+      loop until page.evaluate_script('jQuery.active').zero?
+      @project.reload
+    }.not_to change(@project.projects_datapaths, :count)
+
+    @project.tracks.each do |track|
+      expect(track.projects_datapath).not_to eq @subdir1
+    end
+
+    expect(fancytree_parent("subdir1")[:class]).not_to include 'fancytree-selected'
+    [@datapaths[0].path, "track1.bam", "track2.bam"].each do |title|
+      expect(fancytree_parent(title)[:class]).to include 'fancytree-selected'
+    end
   end
 end
