b32f1d4ae12b2ac6d149d0d585bb23b39ff2d8c6
Luis Gracia <lgraval@gmail.com>
2015-06-22 13:59:57 -0400

    Fix assignment of tracks to projects during migration

--- db/migrate/20141211201053_add_projects_datapath_id_to_tracks.rb
+++ db/migrate/20141211201053_add_projects_datapath_id_to_tracks.rb
@@ -3,10 +3,21 @@ class AddProjectsDatapathIdToTracks < ActiveRecord::Migration
     add_column :tracks, :projects_datapath_id, :integer
 
     if Track.count > 0
+      # Need to find the project directly with sql
+      projects = Hash[Track.connection.select_all("select tracks.id, tracks.project_id from tracks inner join projects on tracks.project_id = projects.id").rows]
+
       Track.reset_column_information
       Track.all.each do |track|
-        pd = ProjectsDatapath.all.select {|pd| track.path.include?(pd.full_path)}.first
-        track.update!(projects_datapath_id: pd.id, path: track.path.gsub(pd.full_path, ''))
+        project = projects[track.id.to_s]
+        pds = ProjectsDatapath.where(project_id: project.to_i).select {|pd| track.path.include?(pd.full_path)}
+        if pds.length > 1
+          raise ActiveRecord::MigrationError, "Multiple datapaths for track #{track.id}: #{pds.inspect}"
+        else
+          pd = pds.first
+          oldpath = track.path
+          track.update!(projects_datapath_id: pd.id, path: track.path.gsub(pd.full_path, ''))
+          say "#{track.id} -- #{pd.id} -- #{project.to_i == pd.project.id} -- #{oldpath == track.full_path}"
+        end
       end
     end
 
