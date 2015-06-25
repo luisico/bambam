eeee42118fcb0cc45640ddcee788dd78b70c653b
John Richardson <richardsonjm@gmail.com>
2015-06-25 09:45:33 -0400

    Fix steps for bip text area element

--- features/projects/step_definitions/edit_description_steps.rb
+++ features/projects/step_definitions/edit_description_steps.rb
@@ -13,18 +13,20 @@ Then /^I should( not)? be able to edit the project description$/ do |negate|
     }
   else
     expect{
-      bip_text(@project, :description, 'new_description')
-      expect(page).to have_content 'new_description'
+      bip_area(@project, :description, 'new_description')
+      click_button 'save'
       loop until page.evaluate_script('jQuery.active').zero?
       @project.reload
     }.to change(@project, :description)
     expect(@project.description).to eq 'new_description'
+    expect(page).to have_content 'new_description'
   end
 end
 
 Then /^I should be able to set project description to blank$/ do
   expect{
-    bip_text(@project, :description, '')
+    bip_area(@project, :description, '')
+    click_button 'save'
     loop until page.evaluate_script('jQuery.active').zero?
     @project.reload
   }.to change(@project, :description)
