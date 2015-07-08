b2ba5bd2bc72cb8940c6938c1cf04e85b938446e
John Richardson <richardsonjm@gmail.com>
2015-07-08 12:05:58 -0400

    Removing selected track parent tests

---
+++ spec/features/fancytree_before_select_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.feature "Before select" do
+  before do
+    @manager = FactoryGirl.create(:manager)
+    @project = FactoryGirl.create(:project, owner: @manager)
+    @datapaths = create_datapaths
+
+    add_user_to_datapaths(@manager, @datapaths)
+
+    sign_in @manager
+  end
+
+  scenario "manager cancels deselection of parent of selected track", js: true do
+    datapath1 = preselect_datapath(@project, @datapaths[0])
+    preselect_track(datapath1, 'track11', 'bam', @manager)
+    visit project_path(@project)
+
+    expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
+    expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
+
+    expect {
+      reject_confirm_from do
+        select_node('datapath1')
+      end
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.not_to change(@project.projects_datapaths, :count)
+
+    expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
+    expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
+  end
+
+  scenario "manager confirms deselection of parent of selected track", js: true do
+    datapath1 = preselect_datapath(@project, @datapaths[0])
+    preselect_track(datapath1, 'track11', 'bam', @manager)
+    visit project_path(@project)
+
+    expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
+    expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
+
+    expect {
+      accept_confirm_from do
+        select_node('datapath1')
+      end
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.to change(@project.projects_datapaths, :count).by(-1)
+
+    expect(fancytree_parent(@datapaths[0].path)[:class]).not_to include 'fancytree-selected'
+    expect(fancytree_parent('track11')[:class]).not_to include 'fancytree-selected'
+  end
+end
