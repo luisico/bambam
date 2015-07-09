aecc9dfdc98b2306d13a4dd9d39249dae32398b8
John Richardson <richardsonjm@gmail.com>
2015-07-09 17:46:17 -0400

    Remove overlap with edit datapath feature

--- spec/features/fancytree_manager_basic_function_spec.rb
+++ spec/features/fancytree_manager_basic_function_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Basic fancytree functions" do
+RSpec.feature "Manager basic fancytree functions" do
   before do
     @manager = FactoryGirl.create(:manager)
     @project = FactoryGirl.create(:project, owner: @manager)
@@ -11,7 +11,7 @@ RSpec.feature "Basic fancytree functions" do
     sign_in @manager
   end
 
-  scenario "manager adds a top level datapath", js: true do
+  scenario "adds a top level datapath", js: true do
     visit project_path(@project)
     expand_node(@datapaths[0].path)
     expect(fancytree_parent('track11.bam')).not_to have_css '.fancytree-checkbox'
@@ -25,7 +25,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track11.bam')).to have_css '.fancytree-checkbox'
   end
 
-  scenario "manager is informed of a failed datapath addition", js: true do
+  scenario "is informed of a failed datapath addition", js: true do
     allow_any_instance_of(ProjectsDatapath).to receive(:valid?).and_return(false)
 
     expect {
@@ -37,7 +37,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_node(@datapaths[0].path).text).to include "Record not created"
   end
 
-  scenario "manager removes a top level datapath", js: true do
+  scenario "removes a top level datapath", js: true do
     preselect_datapath(@project, @datapaths[0])
     visit project_path(@project)
 
@@ -54,7 +54,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track11.bam')).not_to have_css '.fancytree-checkbox'
   end
 
-  scenario "manager is informed of a failed datapath deletion", js: true do
+  scenario "is informed of a failed datapath deletion", js: true do
     preselect_datapath(@project, @datapaths[0])
     allow_any_instance_of(ProjectsDatapath).to receive(:destroy).and_return(false)
 
@@ -67,7 +67,22 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_node(@datapaths[0].path).text).to include "Record not deleted"
   end
 
-  scenario "manager adds a nested datapath", js: true do
+  scenario "can add and immediately remove a datapath", js: true do
+    visit project_path(@project)
+    expect {
+      select_node(@datapaths[0].path)
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.to change(@project.projects_datapaths, :count).by(1)
+    expect(fancytree_parent(@datapaths[0].path)[:class]).to include 'fancytree-selected'
+
+    expect {
+      select_node(@datapaths[0].path)
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.to change(@project.projects_datapaths, :count).by(-1)
+    expect(fancytree_parent(@datapaths[0].path)[:class]).not_to include 'fancytree-selected'
+  end
+
+  scenario "adds a nested datapath", js: true do
     visit project_path(@project)
 
     expect {
@@ -79,7 +94,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('dir11')[:class]).to include 'fancytree-selected'
   end
 
-  scenario "manager removes a nested datapath", js: true do
+  scenario "removes a nested datapath", js: true do
     preselect_datapath(@project, @datapaths[0], 'dir11')
     visit project_path(@project)
 
@@ -96,7 +111,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track111.bam')).not_to have_css '.fancytree-checkbox'
   end
 
-  scenario "manager adds a track to a top level datapath", js: true do
+  scenario "adds a track to a top level datapath", js: true do
     visit project_path(@project)
     select_node(@datapaths[0].path)
     loop until page.evaluate_script('jQuery.active').zero?
@@ -110,7 +125,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
   end
 
-  scenario "manager removes a track from a top level datapath", js: true do
+  scenario "removes a track from a top level datapath", js: true do
     datapath1 = preselect_datapath(@project, @datapaths[0])
     preselect_track(datapath1, 'track11', 'bam', @manager)
     visit project_path(@project)
@@ -125,7 +140,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track11')[:class]).not_to include 'fancytree-selected'
   end
 
-  scenario "manager adds a track to a nested datapath", js: true do
+  scenario "adds a track to a nested datapath", js: true do
     visit project_path(@project)
     expand_node(@datapaths[0].path)
     select_node('dir11')
@@ -140,7 +155,7 @@ RSpec.feature "Basic fancytree functions" do
     expect(fancytree_parent('track111')[:class]).to include 'fancytree-selected'
   end
 
-  scenario "manager removes a track from a nested datapath", js: true do
+  scenario "removes a track from a nested datapath", js: true do
     dir11 = preselect_datapath(@project, @datapaths[0], 'dir11')
     preselect_track(dir11, 'track111', 'bam', @manager)
     visit project_path(@project)
---
+++ spec/features/fancytree_user_basic_function_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.feature "User basic fancytree functions" do
+  before do
+    @user = FactoryGirl.create(:user)
+    @project = FactoryGirl.create(:project, users: [@user])
+    @datapaths = create_datapaths
+
+    add_user_to_datapaths(@project.owner, @datapaths)
+
+    sign_in @user
+  end
+
+  scenario "cannot add a top level datapath", js: true do
+    visit project_path(@project)
+    expect(page).to have_css(".subhead-note", text: "Contact project owner to add/remove datapaths")
+  end
+
+  scenario "cannot add a nested datapath", js: true do
+    preselect_datapath(@project, @datapaths[0])
+    visit project_path(@project)
+    expand_node(@datapaths[0].path)
+    expect(fancytree_parent('dir11')).not_to have_css '.fancytree-checkbox'
+  end
+
+  scenario "adds a track to a datapath", js: true do
+    preselect_datapath(@project, @datapaths[0])
+    visit project_path(@project)
+
+    expect {
+      expand_node(@datapaths[0].path)
+      select_node('track11.bam')
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.to change(@project.tracks, :count).by(1)
+
+    expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
+  end
+
+  scenario "removes a track from a datapath", js: true do
+    datapath1 = preselect_datapath(@project, @datapaths[0])
+    preselect_track(datapath1, 'track11', 'bam', @user)
+    visit project_path(@project)
+
+    expect(fancytree_parent('track11')[:class]).to include 'fancytree-selected'
+
+    expect {
+      select_node('track11.bam')
+      loop until page.evaluate_script('jQuery.active').zero?
+    }.to change(@project.tracks, :count).by(-1)
+
+    expect(fancytree_parent('track11')[:class]).not_to include 'fancytree-selected'
+  end
+
+  scenario "cannot remove another users track from a top level datapath", js: true do
+    datapath1 = preselect_datapath(@project, @datapaths[0])
+    preselect_track(datapath1, 'track11', 'bam', @project.owner)
+    visit project_path(@project)
+
+    expect(fancytree_parent('track11')).not_to have_css '.fancytree-checkbox'
+  end
+end
