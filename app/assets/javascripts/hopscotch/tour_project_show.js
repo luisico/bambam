2ca17dad345a354eea9b636a42064ae32073a988
John Richardson <richardsonjm@gmail.com>
2015-08-17 10:42:31 -0400

    Created-at icon

--- app/assets/javascripts/hopscotch/tour_project_show.js
+++ app/assets/javascripts/hopscotch/tour_project_show.js
@@ -16,7 +16,7 @@ var tour_project_show = {
     },
     {
       title: "Timestamps",
-      content: "Project creation date <i class='fi-calendar icon-tour'></i> and date of last update <i class='fi-clock icon-tour'></i>.",
+      content: "Project creation date <i class='created-at-icon icon-tour'></i> and date of last update <i class='fi-clock icon-tour'></i>.",
       target: "#timestamps",
       placement: 'left'
     },
